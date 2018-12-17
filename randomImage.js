fdf20523affbc2be72a3c9a93df9ab63b932f2ad
Hans Jakob Bollinger <contact@webbear.ch>
2018-12-17 09:40:31 +0100

    randowmimage without aws cred

--- randomImage.js
+++ randomImage.js
@@ -1,138 +1,57 @@
-///////////////////////
-// randomImage() version 1.1
-//////////////////////
 var randomImage = function(params) {
 	var options = $.extend({
 		bucket: null,
 		path: null,
 		awsRegion: "eu-west-1",
-		container: ".random-image",
-	default: null //"/galleries/test1/img1.jpg"
+		container: ".random-image"
 	}, params);
 
-	if (options.bucket !== null && options.path !== null && options.default !== null) {
-
-		var defaultImage = "<img src='" + options.default +"' class='random-image default' alt=''  />";
-
-		var loadScript = function(url, callback) {
-			var head = document.getElementsByTagName("head")[0];
-			var script = document.createElement("script");
-			script.src = url;
-
-			// Attach handlers for all browsers
-			var done = false;
-			script.onload = script.onreadystatechange = function() {
-				if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-					done = true;
-					// Continue your code
-					callback();
-					// Handle memory leak in IE
-					script.onload = script.onreadystatechange = null;
-					head.removeChild(script);
-				}
-			};
-			head.appendChild(script);
-		};
+	if (options.bucket !== null && options.path !== null) {
 
 
-		var awsSDKsupport = function() {
-			var ua = navigator.userAgent.toLowerCase();
-			var out = true;
-			//console.log(ua);
-			if (ua.indexOf('msie') > -1) {
-				//old ie check
-				if (ua.indexOf('msie 10') > -1) {
-					out = true;
-				} else {
-					out = false;
-				}
-			}
-			// chrome check
-			else if (ua.indexOf('chrome') !== -1 && parseFloat(ua.substring(ua.indexOf('chrome') + 7).split(' ')[0]) < 28) {
-				out = false;
-			}
-			// safari check
-			else if (ua.indexOf('safari') !== -1 && ua.indexOf('version') && parseFloat(ua.substring(ua.indexOf('version') + 8).split(' ')[0]) < 5.1) {
-				out = false;
-			}
-			// opera check
-			else if (ua.indexOf('opera') !== -1 && parseFloat(ua.substring(ua.indexOf('version') + 8).split(' ')[0]) < 17) {
-				out = false;
-			}
-			// firefox check
-			else if (ua.indexOf('firefox') !== -1 && parseFloat(ua.substring(ua.indexOf('firefox') + 8)) < 23) {
-				out = false;
-			}
-			return out;
-		};
+		var renderImage = function(bucket, path,  container) {
 
-		var renderImage = function(bucket, path, awsRegion,  container) {
+			var url = "https://s3-eu-west-1.amazonaws.com/" + bucket + "?delimiter=%2F&encoding-type=url&marker=&max-keys=1000&prefix=" + encodeURI(path);
 
-			AWS.config.update({
-				accessKeyId: 'AKIAJ3CTMVV7WBH5CO2Q',
-				secretAccessKey: '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'
-			});
+			$.get(url, function(xml) {
 
-			AWS.config.region = awsRegion;
-
-			var params = {
-				Bucket: bucket, // required /
-				Delimiter: '/',
-				EncodingType: 'url',
-				Marker: '',
-				MaxKeys: 1000,
-				Prefix: path
-			};
-
-			var s3 = new AWS.S3({
-				sslEnabled: true
-			});
-			s3.listObjects(params, function(error, data) {
-				if (error) {
-					$(container).append(defaultImage);
-				} else {
+				if ($(xml).find("Contents").length > 1) {
 					var d = [];
-					// console.log(data);
-					if (data.Contents.length === 0) {
-						$(container).append(defaultImage);
-					} else if (data.Contents.length > 1) {
-						data.Contents.shift();
+					var objects = $(xml).find("Contents");
+					objects.splice(0, 1);
+					var allowed = /^jpg|jpeg|gif|png$/;
 
-						var objects = data.Contents,
-							allowed = /^jpg|jpeg|gif|png$/;
+					$.each(objects, function(k, v) {
+						if ($(v).find("Key").length) {
+                var key = $(v).find("Key").text();
+                // console.log(key);
+                var exten = key.split(".").pop().toLowerCase();
+                if (key.lastIndexOf("__w_") === -1 && allowed.test(exten)) {
+                  d.push(key);
+                }
+              }
+					});
 
-						for (var i = 0, dataL = objects.length; i < dataL; i++) {
-							// console.log(objects[i])
-							var key = objects[i].Key,
-								exten = key.split(".").pop().toLowerCase();
-							if (key.lastIndexOf("__w_") === -1 && allowed.test(exten)) {
-								d.push(objects[i]);
-							}
-						}
-
-						var random = Math.floor((Math.random() * d.length));
-						var randomImage = "<img src='/" + d[random].Key+ "' alt='' class='random-image random" + random + "' />";
+					var random = Math.floor((Math.random() * d.length));
+						var randomImage = "<img src='/" + d[random]+ "' alt='' class='random-image random" + random + "' />";
 						$(container).append(randomImage);
-					}
+
+
+				} else {
+					console.log("Bitte überprüfen Sie den Pfad in den Parameter");
 				}
+
+
+			}).error(function(error) {
+				console.log(error);
 			});
+
 		};
 
-		if (awsSDKsupport()) {
-			if (typeof AWS === 'undefined') {
-				loadScript("https://sdk.amazonaws.com/js/aws-sdk-2.1.27.min.js", function(){
-					renderImage(options.bucket, options.path, options.awsRegion, options.container);
-				});
-			} else {
-				renderImage(options.bucket, options.path, options.awsRegion, options.container);
-			}
-		} else {
-			$(options.container).append(defaultImage);
-		}
+		renderImage(options.bucket, options.path, options.container);
 
 	} else {
-		console.log('please define parameters bucket, path and default like {bucket: "my.bucket.com", path: "random/images/", default: "/random/images/default.jpg"}');
+		console.log('please define parameters bucket, path like {bucket: "my.bucket.com", path: "random/images/"}');
 	}
-
 
 };
